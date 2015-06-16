236e039ee62eabd17b94b57ba9d74489fffd542d
Juhász Ádám <juhasz.adam@virgo.hu>
2015-06-16 11:13:54 +0200

    1.2 - Típusok

---
+++ 1/1.2/Tipusok.php
@@ -0,0 +1,18 @@
+<?php
+echo " *** Néhány alap típus" . PHP_EOL;
+$a = 1; // int
+var_dump($a);
+$a = 1.1; // double
+var_dump($a);
+$a = 'egy'; // string
+var_dump($a);
+$a = [1]; // iteratív tömb
+var_dump($a);
+$a = ["egy" => 1]; // asszociatív tömb
+var_dump($a);
+$a = true; // boolean
+var_dump($a);
+$a = new StdClass; // osztályobjektum
+var_dump($a);
+$a = null; // null
+var_dump($a);
