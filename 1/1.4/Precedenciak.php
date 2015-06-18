48197ff0f09cd85d2a36fd9255294a4b971185bb
Juhász Ádám <juhasz.adam@virgo.hu>
2015-06-18 15:38:03 +0200

    1.4- Különleges Precedencia példák

--- 1/1.4/Precedenciak.php
+++ 1/1.4/Precedenciak.php
@@ -6,14 +6,24 @@ echo (2 * (20 + 1)) . PHP_EOL;
 echo ($a = 2 + $a = 42 - $a = 2) . PHP_EOL;
 
 $bar = true;
-$str = "TEST". $bar ? 'true' : 'false' ."TEST";
-echo PHP_EOL.$str;
+$str = "TEST" . $bar ? 'true' : 'false' . "TEST";
+echo PHP_EOL . $str;
 
 $bar = true;
-$str = "TEST". ($bar ? 'true' : 'false') ."TEST";
-echo PHP_EOL.$str;
+$str = "TEST" . ($bar ? 'true' : 'false') . "TEST";
+echo PHP_EOL . $str;
 
 echo PHP_EOL;
 
 $x = 4;
-echo "x minus one equals " . $x-1 . ", or so I hope";
+echo "x minus one equals " . $x - 1 . ", or so I hope";
+
+echo PHP_EOL;
+
+for ($i = 1; $i < 5; $i++) {
+    $a = $i == 1 ? "one" :
+        $i == 2 ? "two" :
+            $i == 3 ? "three" :
+                "error";
+    echo $a . PHP_EOL;
+}
