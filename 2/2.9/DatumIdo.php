c9f753913d5aa5a5f470b4509cdb2662c6558868
Juhász Ádám <juhasz.adam@virgo.hu>
2015-06-18 16:06:55 +0200

    2.9 Dátum fix: számláló minden napra ;)

--- 2/2.9/DatumIdo.php
+++ 2/2.9/DatumIdo.php
@@ -5,9 +5,11 @@ echo $dateTime->add(new DateInterval("P1W"))->format('Y.m.d') . " lesz egy hét 
 echo $dateTime->add(DateInterval::createFromDateString('7 weekdays'))->format('Y.m.d') . " lesz hét munkanap múlva." . PHP_EOL;
 echo (new DateTime())->format('H:i:s') . " a pontos idő." . PHP_EOL;
 
-$meloVege = new DateTime('2015-06-16 16:30:00');
+$meloVege = (new DateTime())->setTime(16,15);
 
-for ($i = 0; $i <= 600; $i++) {
+
+$maradekIdo = $meloVege->diff(new DateTime());
+for ($i = 0; $maradekIdo->i > 0; $i++) {
     $maradekIdo = $meloVege->diff(new DateTime());
     echo $maradekIdo->format('%H:%I:%S') . PHP_EOL;
     sleep(1);
