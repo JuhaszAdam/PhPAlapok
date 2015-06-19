03311b60c848d2265f173e92cd4e1a20b4511b6d
Juhász Ádám <juhasz.adam@virgo.hu>
2015-06-19 09:54:09 +0200

    1.1 - Példa

--- 1/1.1/Exponentiation.php
+++ 1/1.1/Exponentiation.php
@@ -5,5 +5,10 @@ $a = 2 ** 3 * 2 + 2;
 echo $a . PHP_EOL;
 $a = 2 + 2 * 2 ** 3;
 echo $a . PHP_EOL;
+$a =  2 ** 2 ** 3;  // Jobbról értékelődik. Ez programban érdekesnek tűnhet,
+                    // de ha leírjuk füzetbe fentről lefelé
+                    // haladunk vele, tehát jobbról balra.
+                    // ( 2^(2^3)) = 2^8 = 256
+echo $a . PHP_EOL;
 
 // Precedencia követi az alap matematikai precedenciákat: ( ^ > * > + )
